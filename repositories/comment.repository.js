e8a60642388d76a36d1bd0ba3355d779fb155c41
sch7878 <sch8787@naver.com>
2023-01-16 11:49:52 +0900

    [feat] 댓글 좋아요 기능 추가 [refactor] 댓글부분 모델 마이그레이트 변경

--- repositories/comment.repository.js
+++ repositories/comment.repository.js
@@ -1,53 +1,70 @@
 class CommentRepository {
   //* 의존성 주입
-  constructor(CommentsModel, QuizzesModel, MembersModel, CommentLikesModel, CommentDislikesModel) {
+  constructor(CommentsModel, QuizzesModel, MembersModel, CommentLikesModel) {
     //* Model
     this.commentsModel = CommentsModel;
     this.quizzesModel = QuizzesModel;
     this.membersModel = MembersModel;
     this.commentLikesModel = CommentLikesModel;
-    this.commentDislikesModel = CommentDislikesModel;
   }
 
   //댓글 DB에 생성
   createComment = async (mId, qId, nickname, comment) => {
-
-      const createCommentData = await this.commentsModel.create(
-      {mId, qId, nickname, comment},
-      {where : {qId}}
-    )
+    const createCommentData = await this.commentsModel.create(
+      { mId, qId, nickname, comment },
+      { where: { qId } }
+    );
 
     return createCommentData;
   };
 
   getAllComments = async (qId) => {
-
-    const allComments = await this.commentsModel.findAll({qId});
+    const allComments = await this.commentsModel.findAll({ qId });
 
     return allComments;
   };
 
   updateComment = async (cId, comment) => {
-
     const updateCommentData = await this.commentsModel.update(
-      {comment},
-      {where : {cId}}
+      { comment },
+      { where: { cId } }
     );
 
     return updateCommentData;
-    
   };
 
   deleteComment = async (cId) => {
-
-    const deleteCommentData = await this.commentsModel.destroy(
-      {where : {cId}}
-    );
+    const deleteCommentData = await this.commentsModel.destroy({
+      where: { cId },
+    });
 
     return deleteCommentData;
   };
 
+  findCommentLike = async (cId, mId) => {
+    return await this.commentLikesModel.findOne({ where: { cId, mId } });
+  };
 
+  delteCommentLike = async (cLId) => {
+    return await this.commentLikesModel.destroy({ where: { cLId } });
+  };
+
+  updateCommentLike = async (cLId, commentLikeStatus) => {
+    return await this.commentLikesModel.update(
+      { commentLikeStatus },
+      { where: { cLId } }
+    );
+  };
+
+  createCommentLike = async (cId, mId, commentLikeStatus) => {
+    const data = await this.commentLikesModel.create({
+      cId,
+      mId,
+      commentLikeStatus,
+    });
+
+    return data;
+  };
 }
 
-module.exports = CommentRepository
+module.exports = CommentRepository;
