665dbcb43a60e0b1a96e360ce54690c773be36a7
F1rstID <wndhdks4536@gmail.com>
2023-01-16 22:39:05 +0900

    [refector] API 명세서에 맞게 데이터 형식 수정.

--- controllers/quizzes.controller.js
+++ controllers/quizzes.controller.js
@@ -38,7 +38,7 @@ class QuizzesController {
     //* 모든 게시글 조회.
     const quizzes = await this.quizzesService.getAllQuizzes();
     //* 성공시 200(OK)
-    return res.status(200).json({ quizzes });
+    return res.status(200).json(quizzes);
   };
 
   getQuiz = async (req, res) => {
@@ -46,7 +46,7 @@ class QuizzesController {
     const { qId } = req.params;
     const quiz = await this.quizzesService.getQuiz(qId);
     //* 성공시 200(OK)
-    return res.status(200).json({ quiz: quiz[0] });
+    return res.status(200).json(quiz[0]);
   };
 
   updateQuiz = async (req, res) => {
